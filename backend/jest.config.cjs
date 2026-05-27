b9729b1caf93797cdf0ddbfbe84c477ca2bb7239
Saeed <saeed.m.n1362@gmail.com>
2026-05-27 14:04:15 +0330

    chore: clean turbo cache

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,16 +1,13 @@
 module.exports = {
   preset: "ts-jest",
-
   testEnvironment: "node",
 
   roots: ["<rootDir>/src"],
-
   testMatch: ["**/__tests__/**/*.test.ts"],
 
   moduleFileExtensions: ["ts", "js", "json"],
 
   clearMocks: true,
-
   restoreMocks: true,
 
   setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
@@ -21,14 +18,10 @@ module.exports = {
 
   collectCoverage: true,
   coverageDirectory: "coverage",
-
   coverageReporters: ["text", "lcov", "html"],
 
-  verbose: false,
-
-  detectOpenHandles: false, // ❗ مهم: دیگه لازم نیست با forceExit بجنگی
-
-  forceExit: false, // ❗ حذف کامل اجبار خروج
+  detectOpenHandles: false,
+  forceExit: false,
 
   testTimeout: 15000,
 };
